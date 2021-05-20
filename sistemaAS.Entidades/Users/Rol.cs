f065c4ff63f4b4fd7e5f9db97bf459f96f24799f
kevinT580 <78985620+kevinT580@users.noreply.github.com>
2021-05-19 23:38:59 -0600

    Entidades usuario

--- sistemaAS.Entidades/Users/Rol.cs
+++ sistemaAS.Entidades/Users/Rol.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace sistemaAS.Entidades.Users
 {
-    class Rol
+    public class Rol
     {
+       public int idRol { get; set; }
+       [Required]
+       [StringLength(50, MinimumLength =3, ErrorMessage = "DATO INCORRECTO")]
+       public string nombrRol { get; set; }
+        [StringLength(50)]
+        public string descripcionRon { get; set; }
+        [StringLength(100)]
+        public string condicion { get; set; }
     }
 }
---
+++ sistemaAS.Entidades/Users/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+namespace sistemaAS.Entidades.Users
+{
+   public class Usuario
+    {
+        public int idUsuario { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength =3, ErrorMessage ="DATOS ERRONEOS")]
+        public int idRol { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength =3, ErrorMessage = "DATOS ERRONEOS")]
+        public string nombreUsuario { get; set; }
+        [StringLength(100)]
+        public string tipoDocumento { get; set; }
+        [StringLength(20)]
+        public string numDocumento { get; set; }
+        [StringLength(20)]
+        public string direccion { get; set; }
+        [StringLength(70)]
+        public string telefono { get; set; }
+        [StringLength(20)]
+        public string email { get; set; }
+        [StringLength(50)]
+        public string password_hash { get; set; }
+        public string password_sal { get; set; }
+        public string condicion { get; set; }
+    }
+}
